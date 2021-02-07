3870fae0809cbc2eb49bfd1a132be04a3620df31
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-02-08 00:46:59 +0100

    variables names

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+"use strict";
+
+
+// hoisting
+var age ;
+console.log(age);
+age = 26;
+
+
 console.log("Hello World!");
 
 
@@ -5,8 +14,52 @@ console.log("Hello World!");
 // variables
 
 var firstName = "John";
-console.log(username);
+console.log(firstName);
 
 // 1)  names only contain letters, numbers, symbol $ _
 // 2) first character must not be a number
 
+
+
+// message = "Hello World!";
+// // browser - window
+// // server  - global
+// console.log(window.message);
+
+
+
+
+// 1) sloppy mode - normal mode - default in scripts
+// 2) strict mode - throws more errors, prevents pitfalls
+
+
+// let  / const
+console.log(age);
+
+// Temporal dead zone
+
+// let age = 26;
+// let age = 26;   // will get syntax error for duplicate
+
+
+ const weight = 150;
+ const moonWeight = `You weight  ${weight * 0.165} pounds on the moon`
+ console.log(moonWeight);
+
+
+ // template literal ` `
+
+
+
+let isModalVisisble = true;
+
+if(isModalVisisble) {
+    // do something
+}
+
+
+let isLoading;
+
+let hasPosition;
+
+const COLOR_RED = '#f00';
