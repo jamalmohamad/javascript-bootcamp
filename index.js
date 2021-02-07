49018f72fc2f03b05c5ad8bc93cdc8e0ea040bab
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-02-07 16:16:22 +0100

    feat: add variables

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+
+// camelCased
+// variables
+
+var firstName = "John";
+console.log(username);
+
+// 1)  names only contain letters, numbers, symbol $ _
+// 2) first character must not be a number
+
